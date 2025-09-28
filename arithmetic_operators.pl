bdc771a29fda62d7ce0e67589479f6d267ad1c75
Santhoshi Boyina <boyinasanthoshi12@gmail.com>
2025-09-28 16:40:44 +0530

    Arithmetic Operators in perl

---
+++ arithmetic_operators.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+use feature 'say';
+
+say "5 + 4 = ", 5 + 4;
+say "5 - 4 = ", 5 - 4;
+say "5 * 4 = ", 5 * 4;
+say "6 / 3 = ", 6 / 3;
+say "5 % 4 = ", 5 % 4;
+say "2 ** 4 = ", 2 ** 4
+
+ say "LOG 2 = ", log 2;
+ say "random num (1 - 10) = ", int(1 + rand(11));
+ say "sqrt 16 = ", sqrt 16;
